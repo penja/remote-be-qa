144fc0e54da8c5fbb58a412c4a67585b11473ad0
Andrii Penchuk <penja7@gmail.com>
2025-12-15 19:58:14 +0200

    add separate backend

---
+++ tf-s3_vsc-test/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "penja-terraform-state-bucket"
+    key            = "global/s3/terraform.tfstate"  # Path in S3 where the state file will be stored
+    region         = "us-west-2"
+    encrypt        = true                           # Enables server-side encryption
+    # dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
+  }
+}
